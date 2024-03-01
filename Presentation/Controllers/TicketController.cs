4255503c9eec14e96739a8e4f6f6fd1f3db3eaf2
Gurhan Batmaca <gurhanbtmc@hotmail.com>
2024-03-01 13:14:26 +0300

    minimal api değiştirilip controller eklendi

---
+++ Presentation/Controllers/TicketController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation;
+
+[ApiController]
+[Route("api/")]
+public class TicketController: ControllerBase
+{
+    [HttpGet]
+    [Route("get")]
+    public IActionResult GetTest()
+    {
+        return Ok("Okey");
+    }
+}
