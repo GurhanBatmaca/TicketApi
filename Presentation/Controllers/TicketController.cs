2a27dbe65e3842c96f9acdf419dc0433a9b335db
Gurhan Batmaca <gurhanbtmc@hotmail.com>
2024-03-04 20:23:27 +0300

    pageInfo modeli güncellendi

--- Presentation/Controllers/TicketController.cs
+++ Presentation/Controllers/TicketController.cs
@@ -12,6 +12,7 @@ public class TicketController: ControllerBase
 {
     protected private ITicketService _ticketService;
     protected private IConfiguration _configuration;
+    public int PageSize => Int32.Parse(_configuration["PageSize"]!);
     public TicketController(ITicketService ticketService,IConfiguration configuration)
     {
         _ticketService = ticketService;
@@ -22,29 +23,17 @@ public class TicketController: ControllerBase
     [Route("All")]
     public async Task<IActionResult> GetAllTickets(int page=1)
     {
-        var pageSize = Int32.Parse(_configuration["PageSize"]!);
-        var ticketList = await _ticketService.GetAllTickets(page,pageSize);
-        var totalItems = await _ticketService.GetAllTicketsCount();
-        
-        var pageInfo = new PageInfo {
-            TotalItems = totalItems,
-            ItemPerPage = pageSize,
-            CurrentPage = page,
-            TotalPage = (int)Math.Ceiling((decimal)totalItems/pageSize)
-        };
+        var ticketList = await _ticketService.GetAllTickets(page,PageSize);
+        var totalItems = await _ticketService.GetAllTicketsCount();      
+        var pageInfo = new PageInfo {TotalItems=totalItems,ItemPerPage=PageSize,CurrentPage=page,};
 
-        if(ticketList!.Count > 0)
-            return Ok( new {Tickects = ticketList, PageInfo = pageInfo});
-
-        return BadRequest( new {Message = "Listenin boyutu aşıldı.", PageInfo = pageInfo});
-
+        return Ok( new { Tickects = ticketList, PageInfo = pageInfo } );
     }
 
     [HttpPost]
     [Route("Filter")]
     public async Task<IActionResult> Filter([FromQuery] FilterModel model,int page=1)
     {
-        // var pageSize = Int32.Parse(_configuration["PageSize"]!);
         // var ticketList = await _ticketService.GetTicketsByActivity(page,pageSize,UrlConverter.Convert(activity));
         // var totalItems = await _ticketService.GetTicketsByActivityCount(UrlConverter.Convert(activity));
         
@@ -67,7 +56,6 @@ public class TicketController: ControllerBase
     // [Route("Search")]
     // public async Task<IActionResult> Search(DateTime date,string searcString="",int page=1)
     // {
-    //     var pageSize = Int32.Parse(_configuration["PageSize"]!);
     //     var ticketList = await _ticketService.GetSearchResult(page,pageSize,UrlConverter.Convert(searcString),date);
     //     var totalItems = await _ticketService.GetTicketsByActivityCount(UrlConverter.Convert(searcString));
         
--- Shared/Models/PageInfo.cs
+++ Shared/Models/PageInfo.cs
@@ -4,9 +4,6 @@ public class PageInfo
     public int TotalItems { get; set; }
     public int ItemPerPage { get; set; }
     public int CurrentPage { get; set; }
-    public int TotalPage { get; set; }
-    public int TotalPages()
-    {
-        return (int)Math.Ceiling((decimal)TotalItems/ItemPerPage);
-    }
+    public int TotalPages => (int)Math.Ceiling((decimal)TotalItems/ItemPerPage);
+
 }
