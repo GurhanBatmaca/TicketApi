c0b2cfc8907bac0c230449537409a3237f554e2d
Gurhan Batmaca <gurhanbtmc@hotmail.com>
2024-03-16 14:51:36 +0300

    AdminController ve ProductModel eklendi

---
+++ Presentation/Controllers/AdminController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation;
+
+[ApiController]
+[Route("api/admin")]
+public class AdminController: ControllerBase
+{
+
+    [HttpPost]
+    [Route("addticket")]
+    public async Task<IActionResult> AddTicket()
+    {
+        return Ok();
+    }
+}
---
+++ Shared/Models/TicketModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Shared;
+
+public class TicketModel
+{
+    public int Id { get; set; }
+    public int SeatNumber { get; set; }
+    public int Limit { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public double Price { get; set; }
+    public string ImageUrl { get; set; } = string.Empty;
+    public DateTime EventDate { get; set; }
+    public string Url { get; set; } = string.Empty;
+
+    public int AddressId { get; set; }
+    public int ActivityId { get; set; }
+
+    public List<int>? ArtorsIds { get; set; }
+}
